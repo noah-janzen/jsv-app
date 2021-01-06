4f2849c54d27e2fb3e2b191e417e21eba9be95fa
Philipp Student <philipp.student@web.de>
2021-01-06 17:37:16 +0100

    Implemented chat with display of different thread items and detail view on press, implmented basic detail view of thread

--- screens/thread.tsx
+++ screens/thread.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, FlatList, ActivityIndicator } from 'react-native';
 
-export function Thread() {
+export function Thread({ navigation }) {
     return (
-        <View>
-
-        </View>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.message}>
+                <Text style={styles.threadText}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. </Text>
+                <Text style={styles.threadInfo}>16.01.2020 - 10:34 Uhr</Text>
+            </View>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    message: {
+        padding: 20
+    },
+    threadText: {
+        fontSize: 16,
+        marginBottom: 5
+    },
+    threadInfo: {
+        color: 'gray',
+        fontSize: 15
+    }
+});
