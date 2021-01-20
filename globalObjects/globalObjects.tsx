af6da1a8a3017d65526918623be5328690cd9b75
Noah Janzen <janzen@weiyu-digital.com>
2021-01-20 11:12:03 +0100

    Added localhost as serverURL.

--- globalObjects/globalObjects.tsx
+++ globalObjects/globalObjects.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 const globalObjects = {
     serverURL: 'https://www.jsv-huenxe.de/mock-server',
+    //serverURL: 'http://localhost:3000',
     globalHeader: { 
         headers: {
         Accept: 'application/json',
