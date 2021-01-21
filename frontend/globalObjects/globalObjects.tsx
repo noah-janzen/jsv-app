986880a336234e1538adde698478d305ac05ed66
Philipp Student <philipp.student@web.de>
2021-01-21 12:10:34 +0100

    Added correct ip address for local express server to be reachable from emulator

--- frontend/globalObjects/globalObjects.tsx
+++ frontend/globalObjects/globalObjects.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 const globalObjects = {
-    serverURL: 'https://www.jsv-huenxe.de/mock-server',
-    //serverURL: 'http://localhost:3000',
-    globalHeader: { 
+    //serverURL: 'https://www.jsv-huenxe.de/mock-server',
+    serverURL: 'http://10.0.2.2:3000',
+    globalHeader: {
         headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        'Cache-Control': 'no-cache',
-        'Pragma': 'no-cache',
-        'Expires': '0'
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            'Cache-Control': 'no-cache',
+            'Pragma': 'no-cache',
+            'Expires': '0'
         }
     }
 };
